7e645a0e3c522ece8a9cb5b850b850dd09a6bf26
Natalya Makarevich <mak.nathalie@gmail.com>
2021-08-14 12:07:58 +0300

    add test 100% covered

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,16 @@ class StatisticsServiceTest {
 
     assertEquals(expected, actual);
   }
+
+  @Test
+  void findMaxNotFirst() {
+    StatisticsService service = new StatisticsService();
+
+    long[] incomesInBillions = {12, 5, 8, 4, 5, 13, 8, 6, 11, 11, 10};
+    long expected = 13;
+
+    long actual = service.findMax(incomesInBillions);
+
+    assertEquals(expected, actual);
+  }
 }
